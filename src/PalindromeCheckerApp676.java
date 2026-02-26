1c7510b1a4a11013f3212fa77b5dcd31d31ff5ed
ashigupta1103 <ashiguptaa.11@gmail.com>
2026-02-26 11:02:16 +0530

    UC8:LinkedList

--- src/PalindromeCheckerApp676.java
+++ src/PalindromeCheckerApp676.java
@@ -1,25 +1,78 @@
-import java.util.*;
+import org.w3c.dom.Node;
 
 public class PalindromeCheckerApp676{
-     public static void main(String[] args) {
-         String input = "refer";
-         Deque<Character> deque = new ArrayDeque<>();
-            for (int i = 0; i < input.length(); i++) {
-                deque.addLast(input.charAt(i));
-            }
-            boolean isPalindrome = true;
-            while (deque.size() > 1) {
-                char front = deque.removeFirst();
-                char rear = deque.removeLast();
-                if (front != rear) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-            if (isPalindrome) {
-                System.out.println(input + " is a Palindrome.");
-            } else {
-                System.out.println(input + " is NOT a Palindrome.");
-            }
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
         }
     }
+    public static void main(String[] args) {
+        String input = "madam";
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < input.length(); i++) {
+            Node newNode = new Node(input.charAt(i));
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        // Check palindrome
+        boolean isPalindrome = checkPalindrome(head);
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+    }
+
+    // Palindrome logic using fast/slow pointer + in-place reversal
+    public static boolean checkPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Find middle using Fast & Slow pointers
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        // Compare first half and reversed second half
+        Node firstHalf = head;
+        Node secondHalf = prev;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data)
+                return false;
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+}
