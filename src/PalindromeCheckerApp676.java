4004ce36945717358a4abe3b5f9bd251d709beb5
ashigupta1103 <ashiguptaa.11@gmail.com>
2026-02-26 10:45:41 +0530

    UC6:Queue amd Stack

--- src/PalindromeCheckerApp676.java
+++ src/PalindromeCheckerApp676.java
@@ -7,25 +7,19 @@ public class PalindromeCheckerApp676{
         String input = "civic";
         Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
-
-        // Enqueue and Push characters
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
-            queue.add(ch);   // Enqueue (FIFO)
-            stack.push(ch);  // Push (LIFO)
+            queue.add(ch);
+            stack.push(ch);
         }
 
         boolean isPalindrome = true;
-
-        // Compare Dequeue vs Pop
         while (!queue.isEmpty()) {
             if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
         }
-
-        // Print result
         if (isPalindrome) {
             System.out.println(input + " is a Palindrome.");
         } else {
