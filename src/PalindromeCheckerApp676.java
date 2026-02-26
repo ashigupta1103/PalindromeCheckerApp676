16e5801dd9cd907c0476a44bd182655d19dbbe92
ashigupta1103 <ashiguptaa.11@gmail.com>
2026-02-26 11:21:57 +0530

    UC:POerformance

--- src/PalindromeCheckerApp676.java
+++ src/PalindromeCheckerApp676.java
@@ -4,74 +4,62 @@ import java.util.Stack;
 
 public class PalindromeCheckerApp676{
     public static void main(String[] args) {
-        String input = "rotor";
+        String input = "A man a plan a canal Panama".replaceAll("\\s+", "").toLowerCase();
 
-        PalindromeStrategy stackStrategy = new StackStrategy();
-        PalindromeContext context = new PalindromeContext(stackStrategy);
-        System.out.println("Stack Strategy: " + input + " -> " + context.executeStrategy(input));
+        long start, end;
 
-        // Use Deque-based strategy
-        PalindromeStrategy dequeStrategy = new DequeStrategy();
-        context.setStrategy(dequeStrategy);
-        System.out.println("Deque Strategy: " + input + " -> " + context.executeStrategy(input));
+        start = System.nanoTime();
+        boolean result1 = twoPointerPalindrome(input);
+        end = System.nanoTime();
+        System.out.println("Two-pointer Iterative: " + result1 + " | Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        boolean result2 = stackPalindrome(input);
+        end = System.nanoTime();
+        System.out.println("Stack-based: " + result2 + " | Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        boolean result3 = dequePalindrome(input);
+        end = System.nanoTime();
+        System.out.println("Deque-based: " + result3 + " | Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        boolean result4 = recursivePalindrome(input, 0, input.length() - 1);
+        end = System.nanoTime();
+        System.out.println("Recursive: " + result4 + " | Time: " + (end - start) + " ns");
     }
-}
 
-interface PalindromeStrategy {
-    boolean isPalindrome(String input);
-}
+    private static boolean twoPointerPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
 
-// Stack-based strategy
-class StackStrategy implements PalindromeStrategy {
-
-    @Override
-    public boolean isPalindrome(String input) {
+    private static boolean stackPalindrome(String str) {
         Stack<Character> stack = new Stack<>();
-        for (char ch : input.toCharArray()) {
-            stack.push(ch);
-        }
-
-        for (char ch : input.toCharArray()) {
-            if (ch != stack.pop()) {
-                return false;
-            }
-        }
+        for (char ch : str.toCharArray()) stack.push(ch);
+        for (char ch : str.toCharArray())
+            if (ch != stack.pop()) return false;
         return true;
     }
-}
 
-// Deque-based strategy
-class DequeStrategy implements PalindromeStrategy {
-
-    @Override
-    public boolean isPalindrome(String input) {
+    private static boolean dequePalindrome(String str) {
         Deque<Character> deque = new ArrayDeque<>();
-        for (char ch : input.toCharArray()) {
-            deque.addLast(ch);
-        }
-
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                return false;
-            }
-        }
+        for (char ch : str.toCharArray()) deque.addLast(ch);
+        while (deque.size() > 1)
+            if (deque.removeFirst() != deque.removeLast()) return false;
         return true;
     }
-}
 
-class PalindromeContext {
-
-    private PalindromeStrategy strategy;
-
-    public PalindromeContext(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public void setStrategy(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public boolean executeStrategy(String input) {
-        return strategy.isPalindrome(input);
+    private static boolean recursivePalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+        if (str.charAt(start) != str.charAt(end)) return false;
+        return recursivePalindrome(str, start + 1, end - 1);
     }
 }
