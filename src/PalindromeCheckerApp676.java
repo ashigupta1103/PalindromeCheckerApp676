ac8180390dfd5d4a36ff76dce70d85fc6eb6cba5
ashigupta1103 <ashiguptaa.11@gmail.com>
2026-02-26 10:29:23 +0530

    UC6:Queue and Stack

--- src/PalindromeCheckerApp676.java
+++ src/PalindromeCheckerApp676.java
@@ -1,18 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class PalindromeCheckerApp676{
     public static void main(String[] args) {
-        String input = "radar";
-        char[] characters = input.toCharArray();
-        int start = 0;
-        int end = characters.length - 1;
+        String input = "civic";
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        // Enqueue and Push characters
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            queue.add(ch);   // Enqueue (FIFO)
+            stack.push(ch);  // Push (LIFO)
+        }
+
         boolean isPalindrome = true;
-        while (start < end) {
-            if (characters[start] != characters[end]) {
+
+        // Compare Dequeue vs Pop
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
+
+        // Print result
         if (isPalindrome) {
             System.out.println(input + " is a Palindrome.");
         } else {
