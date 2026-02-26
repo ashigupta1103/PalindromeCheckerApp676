a203b26afeea8d3af939cc7d8b10141370c9fdda
ashigupta1103 <ashiguptaa.11@gmail.com>
2026-02-26 09:58:33 +0530

    UC4:Character Array

--- src/PalindromeCheckerApp676.java
+++ src/PalindromeCheckerApp676.java
@@ -1,13 +1,22 @@
-public class PalindromeCheckerApp676 {
-    public static void main(String[] args){
-    String original = "level";
-    String reversed = "";
-    for (int i=original.length()-1;i>=0;i--){
-        reversed = reversed + original.charAt(i);
+public class PalindromeCheckerApp676{
+    public static void main(String[] args) {
+        String input = "radar";
+        char[] characters = input.toCharArray();
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
     }
-    if (original.equals(reversed)){
-        System.out.println(original + " is a Palindrome.");
-    } else {
-        System.out.println(original + " is NOT a Palindrome.");
-    }
-}}
+}
