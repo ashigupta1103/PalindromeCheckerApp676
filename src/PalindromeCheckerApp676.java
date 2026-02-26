933ca05ab345e5345e9e846c7dd33ea37370f58e
ashigupta1103 <ashiguptaa.11@gmail.com>
2026-02-26 10:22:21 +0530

    UC5:Stack Based PC

--- src/PalindromeCheckerApp676.java
+++ src/PalindromeCheckerApp676.java
@@ -1,22 +1,24 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp676{
     public static void main(String[] args) {
-        String input = "radar";
-        char[] characters = input.toCharArray();
-        int start = 0;
-        int end = characters.length - 1;
+    String input="noon";
+        Stack<Character> stack1= new Stack<>();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
         boolean isPalindrome = true;
-        while (start < end) {
-            if (characters[start] != characters[end]) {
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
         if (isPalindrome) {
             System.out.println(input + " is a Palindrome.");
         } else {
             System.out.println(input + " is NOT a Palindrome.");
-        }
-    }
+        }}
 }
